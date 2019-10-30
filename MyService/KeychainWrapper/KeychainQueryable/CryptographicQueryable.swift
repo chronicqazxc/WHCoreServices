3db1f89972e519f663bc49d6f486e1baa011db66
Hsiao <whsiao@mc-cnkc-sdr155l.apac.wdpr.disney.com>
2019-10-30 13:43:05 +0800

    Rename framework

--- MyService/KeychainWrapper/KeychainQueryable/CryptographicQueryable.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CryptographicQueryable.swift
-//  MyService
-//
-//  Created by Hsiao, Wayne on 2019/9/29.
-//  Copyright © 2019 Wayne Hsiao. All rights reserved.
-//
-
-import Foundation
---
+++ WHCoreServicesTests/MockObjects/MockURLSession.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import WHCoreServices
+
+class MockURLSession: URLSessionProtocol {
+
+    private (set) var url: URL?
+    var dataTask = MockURLSessionDataTask()
+// swiftlint:disable all
+    func dataTask(with request: URLRequestProtocol,
+                  completionHandler: @escaping (Data?, URLResponse?, Error?) -> Swift.Void) -> URLSessionDataTaskProtocol {
+// swiftlint:enable all
+        url = request.url
+//        completionHandler(nextData, successHttpURLResponse(request: request), nextError)
+        return dataTask
+    }
+}
